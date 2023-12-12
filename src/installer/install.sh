003d4be32dc4d4d3ae4ee3f926928c324dea695d
ryota1207 <122080265+ryota1207@users.noreply.github.com>
2023-12-13 00:15:00 +0900

    インストーラーの作成2

--- src/installer/install.sh
+++ src/installer/install.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-version=$(curl -s "https://ryota1207.github.io/toolbox.sh/src/installer/info/version.txt")
-commands=$(curl -s "https://ryota1207.github.io/toolbox.sh/src/installer/info/commands.txt")
-license=$(curl -s "https://ryota1207.github.io/toolbox.sh/src/installer/info/license.txt")
+#レスポンスエラー判定関数
+check_response_status() {
+
+    # ステータスコードの取得
+    status=$(curl -o /dev/null -w '%{http_code}\n' -s "$1")
+
+    # ステータスコードが200系でない場合はエラー
+    if [[ ! "$status" =~ ^2[0-9][0-9]$ ]]; then
+        printf "\n■■${RED}[エラー]${NC}\n■■GitHabリポジトリからの情報の取得に失敗しました。\n■■詳細は公式ドキュメントをご確認をよろしくお願い致します。\n"
+        exit 1
+    fi
+}
+
+
+RED='\033[0;31m'
+Yellow='\033[0;33m'
+NC='\033[0m'
 
 
 #初期表示
@@ -10,7 +24,52 @@ printf "
 --------------------------------------------------------------
             [ toolbox.sh ]インストーラーへようこそ!!             
 --------------------------------------------------------------
+
 "
+
+# バージョンの選択肢
+options=("beta(試験リリース版)" "main(開発版)" "キャンセル")
+
+# プロンプト表示文字
+PS3=$'\nインストールバージョンをご選択下さい: '
+
+select opt in "${options[@]}"; do
+    case $opt in
+        "beta(試験リリース版)")
+            printf "\n■■${Yellow}[システム]${NC}\n■■beta(試験リリース版)バージョンが選択されました。\n■■GitHabリポジトリより最新情報を取得します。\n\n"
+            select_version="beta"
+            break
+            ;;
+        "main(開発版)")
+            printf "\n■■${Yellow}[システム]${NC}\n■■main(開発版)バージョンが選択されました。\n■■GitHabリポジトリより最新情報を取得します。\n\n"
+            select_version="main"
+            break
+            ;;
+        "キャンセル")
+            printf "\n■■${Yellow}[システム]${NC}\n■■キャンセルが選択されました。インストールを中止します。\n\n"
+            exit 1
+            ;;
+        *) # 不正な値
+            printf "\n■■${RED}[エラー]${NC}\n■■指定外の値です。選択項目よりお選び下さい。\n\n"
+            ;;
+    esac
+done
+
+
+#リリースバージョン情報の取得
+githab_url="https://raw.githubusercontent.com/ryota1207/toolbox.sh/"
+
+version_url="${githab_url}${select_version}/src/installer/info/version.txt"
+version=$(curl -s "${version_url}")
+check_response_status "${version_url}"
+
+commands_url="${githab_url}${select_version}/src/installer/info/commands.txt"
+commands=$(curl -s "${commands_url}")
+check_response_status "${commands_url}"
+
+license_url="${githab_url}${select_version}/src/installer/info/license.txt"
+license=$(curl -s "${license_url}")
+check_response_status "${license_url}"
 
 
 printf "
@@ -20,12 +79,13 @@ printf "
 
 "
 
-echo "このスクリプトを使用してtoolbox.shをインストールします。よろしいですか？ (y/n)"
+echo "上記の内容でtoolbox.shをインストールします。よろしいですか？ (y/n)"
 read -r response
 if [[ ! $response =~ ^[Yy]$ ]]; then
-    echo "インストールを中止しました。"
+    printf "\n■■${Yellow}[システム]${NC}\n■■キャンセルが選択されました。インストールを中止します。\n\n"
     exit 1
 fi
+
 
 #実行ユーザーの確認
 current_user=$(whoami)
@@ -35,7 +95,7 @@ bin=$(true)
 
 #~/binの確認
 if [ ! -d ~/bin ]; then
-    echo "~/binファイルが存在しない為作成を行います。"
+    printf "\n■■${Yellow}[システム]${NC}\n■■~/binファイルが存在しない為、システムにて作成を行います。\n\n"
     mkdir ~/bin
     chmod 700 ~/bin
     chown $current_user:$current_user ~/bin
@@ -54,7 +114,7 @@ if [ -f ~/.bashrc ]; then
     echo ".bashrcが存在します"
     echo "export PATH=$PATH:~/bin/digs" >> ~/.bashrc
 else
-    echo ".bashrcが存在しません"
+    printf "\n■■${Yellow}[システム]${NC}\n■■~/.bashrcファイルが存在しない為、システムにて作成を行います。\n\n"
     echo "" > ~/.bashrc
     #~/binの確認
     if [ "$bin" == false ]; then
